750c83ab957402967d478ceb8adc25f608713745
Himel Das <60639301+thehimel@users.noreply.github.com>
2021-06-06 23:02:13 +0200

    code updated with better efficiency.

--- src/control.py
+++ src/control.py
@@ -1,31 +1,40 @@
 import speech_recognition as sr
 
+global text
+
 
 def recognize():
+    global text
+
     languages = {
         0: 'en-US',
         1: 'en-IN',
         2: 'bn-BD'
     }
 
-    r = sr.Recognizer()
-
-    # Select the microphone as source
+    # Select the microphone as source.
     with sr.Microphone() as source:
-        print('Please say something...')
-        # print(source.list_microphone_names())
-
-        # Get the audio
-        audio = r.listen(source)
-
         try:
-            # Get the text from the audio
-            text = r.recognize_google(audio_data=audio, language=languages[2])
-            print(f'Your speech: {text}')
-        except Exception as e:
+            print('Please say something...')
+            # print(source.list_microphone_names())
+            r = sr.Recognizer()
+
+            # Get the audio from the source within timeout seconds.
+            audio = r.listen(source=source, timeout=2)
+
+            # Get the text from the audio in the specified language.
+            text = r.recognize_google(audio_data=audio, language=languages[1])
+            if text:
+                print(f'Your speech: {text}')
+
+        except sr.WaitTimeoutError as e:
+            recognize()
+
+        except sr.UnknownValueError as e:
             print(e)
+
+    recognize()
 
 
 if __name__ == "__main__":
-    while True:
-        recognize()
+    recognize()
